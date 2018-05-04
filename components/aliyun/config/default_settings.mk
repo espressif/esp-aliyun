2e2351349c20b059a706046f835f724cb433dd39
Wu Jian Gang <wujiangang@espressif.com>
2018-05-04 18:06:30 +0800

    feat(aliyun): Add default config for esp8266

---
+++ components/aliyun/config/default_settings.mk
@@ -0,0 +1,20 @@
+COVERAGE_CMD            := src/scripts/walk_through_examples.sh
+DEFAULT_BLD             := src/configs/config.espressif.esp8266
+POST_FINAL_OUT_HOOK     := Post_Distro
+SUBDIRS                 := directory-not-exist-actually
+
+FEATURE_MQTT_SHADOW         ?= $(FEATURE_MQTT_COMM_ENABLED)
+FEATURE_COAP_DTLS_SUPPORT   ?= $(FEATURE_COAP_COMM_ENABLED)
+FEATURE_MQTT_ID2_AUTH       ?= n
+FEATURE_MQTT_ID2_CRYPTO     ?= n
+FEATURE_OTA_ENABLED         ?= y
+FEATURE_OTA_FETCH_CHANNEL   ?= HTTP
+FEATURE_OTA_SIGNAL_CHANNEL  ?= MQTT
+FEATURE_MQTT_ID2_ENV        ?= online
+FEATURE_MQTT_COMM_ENABLED   ?= n
+
+CONFIG_LIB_EXPORT           ?= static
+
+FEATURE_SUBDEVICE_STATUS    ?= gateway
+
+CFLAGS  += -DFORCE_SSL_VERIFY
