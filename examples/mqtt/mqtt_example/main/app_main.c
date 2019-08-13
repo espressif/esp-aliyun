0531a99b249d4aefdf6b8eb16c824a35f486e9f4
Li Jing Yi <lijingyi@espressif.com>
2019-08-13 11:40:41 +0800

    Update app_main.c

--- examples/mqtt/mqtt_example/main/app_main.c
+++ examples/mqtt/mqtt_example/main/app_main.c
@@ -72,7 +72,8 @@ void app_main()
 
     conn_mgr_init();
     conn_mgr_register_wifi_event(wifi_event_handle);
-	conn_mgr_set_wifi_config_ext((const uint8_t *)EXAMPLE_WIFI_SSID, strlen(EXAMPLE_WIFI_SSID), (const uint8_t *)EXAMPLE_WIFI_PASS, strlen(EXAMPLE_WIFI_PASS));
+	conn_mgr_set_wifi_config_ext((const uint8_t *)EXAMPLE_WIFI_SSID, strlen(EXAMPLE_WIFI_SSID),
+	                                (const uint8_t *)EXAMPLE_WIFI_PASS, strlen(EXAMPLE_WIFI_PASS));
 
     IOT_SetLogLevel(IOT_LOG_INFO);
 
