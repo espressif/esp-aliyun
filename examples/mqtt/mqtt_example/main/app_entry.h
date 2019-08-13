29c953e0e0b3434ae7e7286c221d99504b9bf0c1
Liu Han <liuhan@espressif.com>
2019-08-13 16:55:55 +0800

    Revert "Merge branch 'example/mqtt' into 'feature/update_v3.0.1'"
    This reverts merge request !171

---
+++ examples/mqtt/mqtt_example/main/app_entry.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2015-2018 Alibaba Group Holding Limited
+ */
+#ifndef __APP_ENTRY_H__
+#define __APP_ENTRY_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linkkit_main(int argc, char *argv[]);
+
+#endif
